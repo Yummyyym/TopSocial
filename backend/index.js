27e388481191a188738fc6a3ccb2a1ff7b44126e
Yummyyym <yolandayang0926@gmail.com>
2024-04-06 16:27:28 +0800

    Added message, profile modules

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,17 @@
 //npm init
 //npm i nodemon concurrently（同时启动多个server） -D
 //npm i express cors json-server axios
+
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes/index');
+//create server
+const app = express();
+
+app.use(cors());
+app.use('/api',router);
+
+const port = 80;
+app.listen(port, function(req,res){
+    console.log('Server is running on http://localhost:80')
+})
---
+++ backend/routes/message.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const messageRouter = express.Router();
+
+const {getMessage} = require('../controllers/messageController')
+
+messageRouter.get('/',getMessage)
+
+module.exports = messageRouter;
